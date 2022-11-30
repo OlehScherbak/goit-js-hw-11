7593b5d8303eab7182faeefbea43a72f159199f2
OlehScherbak <103534824+OlehScherbak@users.noreply.github.com>
2022-11-30 11:11:51 +0200

    add smooth scroll

--- src/js/index.js
+++ src/js/index.js
@@ -71,6 +71,16 @@ async function imgSearch(searchedLine, pageNumber) {
         "We're sorry, but you've reached the end of search results."
       );
     }
+
+    // smooth scroll
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 20,
+      behavior: 'smooth',
+    });
   } catch (error) {
     console.log(error);
   }
